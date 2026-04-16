99fcb351accf0ec6968a70633828432196f36e6c
rakib <mdrakib991943@.com>
2026-04-17 00:57:49 +0600

    added pie Dynamic Pie Chart

--- src/app/(public)/status/page.jsx
+++ src/app/(public)/status/page.jsx
@@ -1,52 +1,32 @@
-import { Pie, PieChart, PieLabelRenderProps, PieSectorShapeProps, Sector } from 'recharts';
-import { RechartsDevtools } from '@recharts/devtools';
+"use client"
+
+import { Pie, PieChart } from 'recharts';
+
 
 // #region Sample data
 const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
+  { name: 'Group A', value: 400, fill: '#0088FE' },
+  { name: 'Group B', value: 300, fill: '#00C49F' },
+  { name: 'Group C', value: 300, fill: '#FFBB28' },
+  { name: 'Group D', value: 200, fill: '#FF8042' },
 ];
 
 // #endregion
-const RADIAN = Math.PI / 180;
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: PieLabelRenderProps) => {
-  if (cx == null || cy == null || innerRadius == null || outerRadius == null) {
-    return null;
-  }
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const ncx = Number(cx);
-  const x = ncx + radius * Math.cos(-(midAngle ?? 0) * RADIAN);
-  const ncy = Number(cy);
-  const y = ncy + radius * Math.sin(-(midAngle ?? 0) * RADIAN);
-
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > ncx ? 'start' : 'end'} dominantBaseline="central">
-      {`${((percent ?? 1) * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-
-const MyCustomPie = (props: PieSectorShapeProps) => {
-  return <Sector {...props} fill={COLORS[props.index % COLORS.length]} />;
-};
-
-export default function PieChartWithCustomizedLabel({ isAnimationActive = true }: { isAnimationActive?: boolean }) {
+export default function PieChartWithPaddingAngle() {
   return (
     <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
       <Pie
         data={data}
-        labelLine={false}
-        label={renderCustomizedLabel}
+        innerRadius="80%"
+        outerRadius="100%"
+        // Corner radius is the rounded edge of each pie slice
+        cornerRadius="50%"
         fill="#8884d8"
+        // padding angle is the gap between each pie slice
+        paddingAngle={5}
         dataKey="value"
-        isAnimationActive={isAnimationActive}
-        shape={MyCustomPie}
       />
-      <RechartsDevtools />
+
     </PieChart>
   );
 }
